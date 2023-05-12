8f4682c91f597e9d436f7a1ac0b1f1982ac17fae
Prapul chandra <40701156+0stephen9980@users.noreply.github.com>
2023-05-12 18:11:44 +0000

    MiddleWare changes

---
+++ src/middleware.ts
@@ -0,0 +1,22 @@
+import { createMiddlewareSupabaseClient } from '@supabase/auth-helpers-nextjs'
+import { NextResponse } from 'next/server'
+
+import type { NextRequest } from 'next/server'
+
+export async function middleware(req: NextRequest) {
+  const res = NextResponse.next();
+  const Url = req.nextUrl;
+  if (Url.pathname.startsWith('/admin')) {
+    const supabase = createMiddlewareSupabaseClient<any>({ req, res });
+    const data = await supabase.auth.getSession();
+    if(data)
+      return NextResponse.rewrite(new URL('/admin/login', req.url));
+  }
+  
+  return res
+}
+ 
+// See "Matching Paths" below to learn more
+export const config = {
+  matcher: ['/admin/:path*']
+};
